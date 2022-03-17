7588a4e1b400414dc7c5659edd8d83fc74e56a0d
Le Hiu <lehieudhqt@gmail.com>
2022-03-17 10:08:18 +0700

    Third commit

---
+++ EcommerceAPI/routes/momo.js
@@ -0,0 +1,94 @@
+const router = require("express").Router();
+
+
+router.post("/payment", (req, resp) => {
+    //https://developers.momo.vn/#/docs/en/aiov2/?id=payment-method
+    //parameters
+    console.log(req.body.amount);
+    var partnerCode = "MOMOXAEI20211121";
+    var accessKey = "1PF94ShTgdTqf8OZ";
+    var secretkey = "ZuEm5a834xA0z7oUHNfMj8PTOvAN83gG";
+    var requestId = partnerCode + new Date().getTime();
+    var orderId = requestId;
+    var orderInfo = "alo ";
+    var redirectUrl = "localhost://5000";
+    var ipnUrl = "https://callback.url/notify";
+    // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
+    var amount = req.body.amount;
+    var requestType = "captureWallet"
+    var extraData = ""; //pass empty value if your merchant does not have stores
+
+    //before sign HMAC SHA256 with format
+    //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
+    var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType
+    //puts raw signature
+    console.log("--------------------RAW SIGNATURE----------------")
+    console.log(rawSignature)
+    //signature
+    const crypto = require('crypto');
+    var signature = crypto.createHmac('sha256', secretkey)
+        .update(rawSignature)
+        .digest('hex');
+    console.log("--------------------SIGNATURE----------------")
+    console.log(signature)
+
+    //json object send to MoMo endpoint
+    const requestBody = JSON.stringify({
+        partnerCode: partnerCode,
+        accessKey: accessKey,
+        requestId: requestId,
+        amount: amount,
+        orderId: orderId,
+        orderInfo: orderInfo,
+        redirectUrl: redirectUrl,
+        ipnUrl: ipnUrl,
+        extraData: extraData,
+        requestType: requestType,
+        signature: signature,
+        lang: 'en'
+    });
+    //Create the HTTPS objects
+    const https = require('https');
+    const options = {
+        hostname: 'test-payment.momo.vn',
+        port: 443,
+        path: '/v2/gateway/api/create',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(requestBody)
+        }
+    }
+    //Send the request and get the response
+        
+        req = https.request(options, res => {
+        console.log(`Status: ${res.statusCode}`);
+        console.log(`Headers: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (body) => {
+            console.log('Body: ');
+            console.log(body);
+            console.log('payUrl: ');
+            console.log(JSON.parse(body).payUrl);
+            resp.status(200).json(JSON.parse(body).payUrl);
+        });
+        
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    })
+    
+    
+
+    req.on('error', (e) => {
+        console.log(`problem with request: ${e.message}`);
+    });
+    // write data to request body
+    console.log("Sending....")
+    req.write(requestBody);
+    req.end();
+})
+
+
+
+module.exports = router;
